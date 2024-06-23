0a8a525bdb24ba0203c263dd834296e48a1d28e8
joshua-8 <joshua_phelps@brown.edu>
2024-06-22 17:04:32 -0700

    simple implementation successful!

--- src/main.ino
+++ src/main.ino
@@ -3,7 +3,7 @@
 #include <Wire.h>
 
 JEncoderAS5048bI2C encoder = JEncoderAS5048bI2C(false, 1, 0x40);
-JMotorDriverEsp32HBridge motor = JMotorDriverEsp32HBridge(0, A0, A1);
+JMotorDriverEsp32HBridge motor = JMotorDriverEsp32HBridge(0, MOSI, MISO);
 
 void setup()
 {
@@ -15,8 +15,31 @@ void setup()
 
 void loop()
 {
-    encoder.run();
-    Serial.println(encoder.getVel());
+    float frictionConstant = analogRead(A0) / 4095.0 - .25;
+    float inertiaConstant = analogRead(A1) / 4095.0 - .25;
 
-    motor.set(encoder.getVel() * -0.1);
+    static unsigned long lastMicros = micros();
+    float dt = (micros() - lastMicros) / 1000000.0;
+    lastMicros = micros();
+
+    encoder.run();
+
+    float vel = encoder.getVel();
+    static float lastEncoderVel = vel;
+    float accReading = (vel - lastEncoderVel) / dt;
+
+    float accReadingSmoothingFactor = 0.005;
+    static float acc = 0;
+    acc = accReading * accReadingSmoothingFactor + (1 - accReadingSmoothingFactor) * acc;
+
+    lastEncoderVel = vel;
+
+    float motorOut = 0;
+    motorOut += vel * -frictionConstant;
+    motorOut += acc * -inertiaConstant;
+
+    motor.set(motorOut); // currently assuming voltage is linearly proportional to speed
+
+    Serial.printf("%6.3f %8.3f %5.2f %9.4f %9.4f\n", vel, acc, motorOut, frictionConstant, inertiaConstant);
+    delay(1);
 }
